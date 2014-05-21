50aa409732408a87e889755085a859810c598cde
Teslon <f.baser@me.com>
2014-05-21 23:29:32 +0200

    second commit

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -8,22 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+#pragma mark - IBActions
 
-- (void)didReceiveMemoryWarning
+- (IBAction)clickTheButton:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"This is a Git tutorial");
 }
 
 @end
